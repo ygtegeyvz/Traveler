00147e538d2ef1c84d18b576e9bd2a39b734d839
ulassatr <ulassatr@gmail.com>
2018-02-28 21:56:42 +0300

    v0.3
    İndirgeme işleminin ilk aşaması tamamlandı.

--- GezelimGorelim/Controllers/MiningController.cs
+++ GezelimGorelim/Controllers/MiningController.cs
@@ -2,6 +2,7 @@
 using GezelimGorelim.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -16,27 +17,78 @@ namespace WebAPI.Controllers
         [HttpGet]
         public List<Location> nbr()
         {
+            bir();
+            reduction();
             LocCont.Get().RemoveAt(1);
+
             return LocCont.Get();
         }
 
-    //    //public List<Location> reduction()
-    //    //{
-    //    //    float x, y;
+        List<Location> location = new List<Location>();
 
-    //    //    List<float> points = new List<float>();
-    //    //    List<float> afterPoint = new List<float>();
+        //[Route("api/Reduction")]
+        //[HttpGet]
+        //public List<Location> reduction()
+        //{
+        //    location = LocCont.Get();
 
-    //    //    float pay = (y - points[points.Count]) * (points[points.Count] - points[0]);
-    //    //    float payda = (points[points.Count] - points[0]) * (x - points[points.Count]);
+        //    return location;
 
-    //    //    if (pay / payda < 3)
-    //    //    {
+        //}
 
-    //    //        afterPoint.Add(points);
+        List<string> PointsX = new List<string>();
+        List<string> PointsY = new List<string>();
+        List<float> PointXFloat = new List<float>();
+        List<float> PointYFloat = new List<float>();
 
-    //    //    }
+        public void bir()
+        {
+            for (int i = 0; i < LocCont.Get().Count; i++)
+            {
+                PointsX.Add(LocCont.Get().ElementAt(i).locationsX);
+                PointsY.Add(LocCont.Get().ElementAt(i).locationsY);
 
-    //}
-}
+            }
+
+            for (int i = 0; i < PointsX.Count; i++)
+            {
+               PointXFloat.Add(float.Parse(PointsX[i],
+                         CultureInfo.InvariantCulture.NumberFormat));
+                PointYFloat.Add(float.Parse(PointsY[i],
+                         CultureInfo.InvariantCulture.NumberFormat));
+
+            }
+
+        }
+
+
+        public void reduction()
+        {
+
+          //  float x, y;
+            List<float> afterPointX = new List<float>();
+            List<float> afterPointY = new List<float>();
+
+            double a = PointXFloat[PointXFloat.Count-1] - PointXFloat[0];
+            double b = PointYFloat[PointYFloat.Count-1] - PointYFloat[0];
+            double boy = Math.Sqrt((a * a) + (b * b));
+
+            for (int i = 0; i < PointsX.Count; i++)
+            {
+                float pay = (PointYFloat[i] - PointYFloat[PointYFloat.Count-1]) * (PointXFloat[PointXFloat.Count-1] - PointXFloat[0]);
+                float payda = (PointYFloat[PointYFloat.Count-1] - PointYFloat[0]) * (PointXFloat[i] - PointXFloat[PointXFloat.Count-1]);
+                              
+                
+                double kontrol =   Math.Abs(pay/payda) / boy;
+
+                if(kontrol>60 && kontrol < 80)
+                {
+                    afterPointX.Add(PointXFloat[i]);
+                    afterPointY.Add(PointYFloat[i]);
+                }
+
+            }
+
+        }
+    }
 }
